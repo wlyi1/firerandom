114bee8006a2c2abd7031576434ac6352ebde40d
waliyudin sammadikun <waliyudin938@gmail.com>
2022-10-18 07:59:42 +0700

    remove import

--- app.py
+++ app.py
@@ -128,25 +128,6 @@ html(''' <html>
   const dbf = getFirestore(app);
 </script>
 <script>
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
-import { getFirestore } from "firebase/firestore"
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDIXll1IVcHQM4dwmovHYRyQm47R-eCIHc",
-  authDomain: "testrandom1-6cf06.firebaseapp.com",
-  projectId: "testrandom1-6cf06",
-  storageBucket: "testrandom1-6cf06.appspot.com",
-  messagingSenderId: "326026321843",
-  appId: "1:326026321843:web:456ef360e512307dd6b5c6",
-  measurementId: "G-KFB1W9K2CM"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
