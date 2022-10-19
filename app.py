a287b0921725a585cdbddd8716e1cb8fc0c3078c
waliyudin sammadikun <waliyudin938@gmail.com>
2022-10-19 12:21:25 +0700

    subscript

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ data = {'lat': [lat], 'lon': [long]}
 dfmap = pd.DataFrame(data)
 st.write(dfmap)
 st.map(dfmap)
-components.iframe(width=600,  height=450, src=f"https://www.google.com/maps/embed/v1/place?key={gmaps}&q=' {lat} +',' + {long}")
+components.iframe(width=600,  height=450, src=f"https://www.google.com/maps/embed/v1/place?key={gmaps}&q=' {lat} +',' + {long}'")
