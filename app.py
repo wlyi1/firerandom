a31ee923d2ae35ffa20b5d26ac2b50d739acbc42
waliyudin sammadikun <waliyudin938@gmail.com>
2022-10-19 13:56:34 +0700

    tabs

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ image3 = Image.open(BytesIO(resp.content))
 #Data Sources
 data = pd.read_csv('rand_aktivitas.csv')
 image1 = 'https://raw.githubusercontent.com/wlyi1/random/main/Random/a3a.png'
-st.image(image1)
+
 
 list_rand = data.name
 tgl_random = datetime.datetime.now()
@@ -62,84 +62,93 @@ for i in word:
     img.text((80,xc), i, font=font, fill=(0,0,0))
     xc += 55
 
-st.caption('Jangan lupa tag @randomku dan pake #random #randomku biar tau cerita menarikmu apa hari ini 🤣')
+tab1, tab2, tab3 = st.tabs(['Kegiatan Random ⏳', 'Makan Terserah 🍵', 'Pilih Orang 😮‍💨'])
+with tab1:
 
-col = db.collection('waliy')
-if st.button('Randomin'):
-    st.image(image3)
-    col.add({'Tanggal' : tgl_random, 'Random' : today_rand})
-    st.balloons()
+    st.image(image1)
+    st.caption('Jangan lupa tag @randomku dan pake #random #randomku biar tau cerita randommu hari ini 🤣')
 
-
-st.markdown("----", unsafe_allow_html=True)
-
-db1 = firestore.Client(credentials=creds, project="testrandom1-6cf06")
-col1 = db.collection('Story_waliy')
-
-with st.form("my_form"):
-    st.write("Ceritain ke RandomKu dong tentang aktivitas randommu hari ini 😃")
-    nama = st.text_input("Namanya? 🧑 👩")
-    cerita = st.text_area("Cerita singkatnya gimana nih? ✍🏻")
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        col1.add({"Tanggal": tgl_random, "Cerita": cerita})
-        st.write('Terimakasih 👍')
-# If the user clicked the submit button. write the data from the form to the database.
-# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+    col = db.collection('waliy')
+    if st.button('Randomin'):
+        st.image(image3)
+        col.add({'Tanggal' : tgl_random, 'Random' : today_rand})
+        st.balloons()
 
 
+    st.markdown("----", unsafe_allow_html=True)
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    db1 = firestore.Client(credentials=creds, project="testrandom1-6cf06")
+    col1 = db.collection('Story_waliy')
 
-    
-if st.checkbox("Check my location"):
-    loc = get_geolocation()
-    with st.spinner('waiting'):
-        time.sleep(3)
-
-    #st.write(f"Your coordinates are {loc}")
-    #st.write(loc['coords']['latitude'])
-    #st.write(loc['coords']['longitude'])
-
-lat_user = loc['coords']['latitude']
-long_user = loc['coords']['longitude']
+    with st.form("my_form"):
+        st.write("Ceritain ke RandomKu dong tentang aktivitas randommu hari ini 😃")
+        nama = st.text_input("Namanya? 🧑 👩")
+        cerita = st.text_area("Cerita singkatnya gimana nih? ✍🏻")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            col1.add({"Tanggal": tgl_random, "Cerita": cerita})
+            st.write('Terimakasih 👍')
+    # If the user clicked the submit button. write the data from the form to the database.
+    # You can store any data you want here. Just modify that dictionary below (the entries between the {}).
 
 
-gmaps = st.secrets['gmaps']
-foods = ['rumah+makan', 'pecel', 'nasi+goreng', '']
 
-url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat_user}%2C{long_user}&radius=1500&types=restaurant&language=id&key={gmaps}"
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-payload = {}
-headers = {}
+with tab2:
+    st.header('Yang mau makan masih bilang terserah, sini random kan aja pilihan makannya!')
+    st.write('nyalain dulu GPS nya dan izinkan')
+    if st.checkbox("Cek Lokasiku"):
+        loc = get_geolocation()
+        with st.spinner('waiting'):
+            time.sleep(5)
 
-response = requests.request("GET", url, headers = headers, data = payload)
-res = response.json()
-#st.write(res)
-total = len(res['results'])
-num = [x for x in range(total)]
-#st.write(num)
-ran_num = random.choice(num)
+        #st.write(f"Your coordinates are {loc}")
+        #st.write(loc['coords']['latitude'])
+        #st.write(loc['coords']['longitude'])
 
-lat = res['results'][ran_num]['geometry']['location']['lat']
-long = res['results'][ran_num]['geometry']['location']['lng']
+    lat_user = loc['coords']['latitude']
+    long_user = loc['coords']['longitude']
 
-lis = []
-for i in range(total):
-    j = res['results'][i]['name']
-    lis.append(j)
 
-#st.write(lat)
-#st.write(lis)
-st.subheader(lis[ran_num])
-data = {'lat': [lat], 'lon': [long]}
-dfmap = pd.DataFrame(data)
-#st.write(dfmap)
-#st.map(dfmap)
-components.iframe(width=600,  height=450, src=f"https://www.google.com/maps/embed/v1/place?key={gmaps}&q={lat}, {long}")
+    gmaps = st.secrets['gmaps']
+    foods = ['rumah+makan', 'pecel', 'nasi+goreng', '']
+
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat_user}%2C{long_user}&radius=1500&types=restaurant&language=id&key={gmaps}"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers = headers, data = payload)
+    res = response.json()
+    #st.write(res)
+    total = len(res['results'])
+    num = [x for x in range(total)]
+    #st.write(num)
+    ran_num = random.choice(num)
+
+    lat = res['results'][ran_num]['geometry']['location']['lat']
+    long = res['results'][ran_num]['geometry']['location']['lng']
+
+    lis = []
+    for i in range(total):
+        j = res['results'][i]['name']
+        lis.append(j)
+
+    #st.write(lat)
+    #st.write(lis)
+    st.subheader(lis[ran_num])
+    data = {'lat': [lat], 'lon': [long]}
+    dfmap = pd.DataFrame(data)
+    #st.write(dfmap)
+    #st.map(dfmap)
+    components.iframe(width=600,  height=450, src=f"https://www.google.com/maps/embed/v1/place?key={gmaps}&q={lat}, {long}")
+
+with tab3:
+    st.write('pilih nama secara random')
